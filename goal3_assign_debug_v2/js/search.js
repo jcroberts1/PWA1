b80e1e530e2beb5abad0742fd619b59df9469727
jcroberts1 <jcroberts1@fullsail.edu>
2014-12-01 12:36:36 -0600

    ANALYZE Buggy Search v1
    ANALYZE Buggy Search v1

--- goal3_assign_debug_v2/js/search.js
+++ goal3_assign_debug_v2/js/search.js
@@ -1,51 +1,51 @@
 /* Job Roberts
 28 November 2014
 PWA1
-
+Comments added
  */
 
  // Create privatized scope using a self-executing function
-function query{                                                               //Self executing function Main Function() run
+(function() query{
+                                                          //Self executing function Main Function() run
 
-};
+
 
 	// Variable initialization (DO NOT FIX ANY OF THE BELOW 'Variables')
-	var resultsDIV = document.getElementById("results");                 //Result variable is declared
-		searchInput = document.forms[0].search;                          //Declare search form
-		currentSearch = ('');                                            //The form will be empty until user inputs a search query
+	var resultsDIV = document.getElementById("results"),                 //Result variable is declared
+		searchInput = document.forms[0].search,                          //Declare search form
+		currentSearch = ''                                           //The form will be empty until user inputs a search query
 
 
 	// Validates search query
-	var validate=function(query){                                         // Validation variable declared
-    };
-}
+	var validate==function(query){                                         // Validation variable declared
+
 
 		// Trim whitespace from start and end of search query
-		while(query.charAt(0) = " "){                                    //
-			query = query.substring(1, query.length);                    // Start character extraction position (1)
+		while(query.charAt(0) = " ") {                                    //
+            query = query.substring(1, query.length);                    // Start character extraction position (1)
+        };
+		while(query.charAt(query.length-1) === ""){
+            query = query.substring(0, query.length-1);                  //
 
-		else(query.charAt(query.length-1) === "")
-            {                      //
-                query = query.substring(0, query.length - 1);                  //
-            }
+        ;
 
 		// Check search length, must have 3 characters
-		if(query.length < 3){                                             //Checks o validate character length
-			alert("Your search query is too small, try again.");          // User will be alerted that the length of the search is to small
+		if(query.length < 3) {                                             //Checks o validate character length
+            alert("Your search query is too small, try again.");          // User will be alerted that the length of the search is to small
 
-			// (DO NOT FIX THE LINE DIRECTLY BELOW)
-			searchInput.focus();                                           // Allows user to enter a new search
+            // (DO NOT FIX THE LINE DIRECTLY BELOW)
+            searchInput.focus();                                           // Allows user to enter a new search
+            return;
+        };
 
-		}
-            return sea                                                     //Will alert user the return of the search from database
-
-		search(query);                                                     //Search initialized to database
-	}
+                                                                 //Will alert user the return of the search from database
+        search(query);                                                     //Search initialized to database
+	};
 
 	// Finds search matches
-	var search = function(query);{                                          // Search variable declared
+	var search = function(query)                                        // Search variable declared
 
-}
+
 
 		// split the user's search query string into an array
 		var queryArray = query.join(" ");                                   //Get query function
@@ -59,21 +59,21 @@ function query{                                                               //
 			// each db[i] is a single video item, each title ends with a pipe "|"
 			// save a lowercase variable of the video title
 			var dbTitleEnd = db[i].indexOf('|');                                   //Data base title has ended.
-			var dbitem = db[i].toLowerCase()().substring(0, dbTitleEnd);          //Database variable declared ,index the database
+			var dbitem = db[i].toLowerCase().substring(0, dbTitleEnd);          //Database variable declared ,index the database
 
 			// loop through the user's search query words
 			// save a lowercase variable of the search keyword
 			for(var ii=0, jj=queryArray.length; ii<jj; ii++){                //Query will determine Character length
-				var qitem = queryArray[ii].toLowerCase()();                  // Get query array
+				var qitem = queryArray[ii].toLowerCase();                  // Get query array
 
 				// is the keyword anywhere in the video title?
 				// If a match is found, push full db[i] into results array
 				var compare = dbitem.indexOf(qitem);                              //Search query will be compared to in the database
 				if(compare !== -1){                                               // Compares search throughout the database
 					results.push(db[i]);                                           //Print the results from database
-				}
-
-
+				};
+            ;
+        ;
 
 		results.sort();                                                             //Invoke result function print results from database
 
@@ -82,14 +82,14 @@ function query{                                                               //
 			noMatch();
 		}else{                                                                      // Else if the results have a match print the matches!!
 			showMatches(results);                                                   //Prints out the results
-		}
-	}
+		};
+	};
 
 	// Put "No Results" message into page (DO NOT FIX THE HTML VAR NOR THE innerHTML)
 	var noMatch = function(){                                                         //No match variable declared run function to browser
-		//noinspection UnnecessaryLocalVariableJS
-        var html = ''+                                                                 //browser variable declared
-			<p>No Results found.</p> //If function return no results print paragraph
+    var html = ''+	                       //noinspection UnnecessaryLocalVariableJS
+                                                                    //browser variable declared
+			''<p>No Results found.</p>'+
             //Print message to user
 			<p style="font-size:10px;">Try searching for "JavaScript" Just an idea"</p>
 		;
@@ -118,7 +118,7 @@ function query{                                                               //
 
 			// make the video link - THE NEXT LINE IS CORRECT.
 			html += '<p><a href=' + url + '>' + title + '</a></p>';// Each result will be linked to a URL
-		}
+		};
 		resultsDIV.innerHTML = html; //THIS LINE IS CORRECT.
 	};
 
@@ -131,6 +131,6 @@ function query{                                                               //
         // return false is needed for most events - this will be reviewed in upcoming course material
         // THE LINE DIRECTLY BELOW IS CORRECT
 		return false;                     //If incorrect the return will print false
-
+    ;
 
 })();   //End program
