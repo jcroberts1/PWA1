a883dae1860a3583aff007617af7764dd59baf52
jcroberts1 <jcroberts1@fullsail.edu>
2014-12-03 19:47:04 -0600

    Duel Version 2
    Updated graphics to duel

---
+++ goal1_assign_duel_V2/js/main.js
@@ -0,0 +1,103 @@
+/**JOB ROBERTS
+ * PWA1
+ * Duel Fight Game - Complete
+ * Date: 11/27/14
+
+*/
+//CONSOLE LOG START PROGRAM
+// self-executing function
+(function(){
+
+    console.log("FIGHT!!!");                        // Display to the Console: "FIGHT"!!(start program)
+
+    //Fighter's damage and health using Arrays!
+    var soilder1 = ["BuddyBoy",20,100];                    // Fighter #1  name variable declared
+    var soilder2 = ["Coda",20,100];                   // Fighter #2 name Variable declared
+
+    //player damage
+    //var player1Damage = 20;                        // Variable for damage to be deducted from fighter#1
+    //var player2Damage = 20;                        // Variable for damage to be deducted from fighter#2
+
+    //player health
+   // var playerOneHealth = 100;                     // Variable Declared for begging Health for Fighter #1
+    //var playerTwoHealth = 100;                     // Variable Declared for begging Health for Fighter #2
+
+    //initiate round variable declared
+    var round=0;
+
+    // After user clicks "OK"button run the main function of the game and loop through each round.
+    function fight(){
+        alert(soilder1[0]+soilder1[2]+"  *START*  "+soilder2[0]+soilder2[2]); // Pop-up player #1 & player #2 name and health
+
+        //Loop through the rounds up to 10 rounds; If i < 10 continue to next round
+        for (var i = 0; i < 10; i++)
+        {
+           //Defined damage using Arrays!
+           var minDamage1 = soilder1[1] * .5;
+           var minDamage2 = soilder2[1] * .5;
+           var f1 = Math.floor(Math.random()*(soilder1[1]-minDamage1)+minDamage1);
+           var f2 = Math.floor(Math.random()*(soilder2[1]-minDamage2)+minDamage2);
+
+
+            //random formula is - Math.floor(Math.random() * (max - min) + min);
+            //var minDamage1 = player1Damage * .5;                    //Damage variable declared for player #1
+            //var minDamage2 = player2Damage * .5;                    //Damage Variable declared for player #2
+            //var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1); //variable declares random number health for player#1
+            //var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2); //variable declares random number health for player#2
+
+
+           //Inflict Damage using Arrays!
+            soilder1[2]-=f1;
+            soilder2[2]-=f2;
+
+
+
+            //inflict damage
+           // playerOneHealth-=f1; //playerOne health =playerOneHealth-f1
+            //playerTwoHealth-=f2; //playerTwoHealth=playerTwoHealth-f2
+
+            //Console.log to check on the progress of each player
+            console.log(soilder1[2]+" "+soilder2[2]);
+
+            //check for victor
+            var result = winnerCheck();                    // Check to see who the winner is
+            console.log(result);                          // Display the result to the console
+
+            //Was there a winner in "Winner Check"? If there was no winner end game Else continue to next round
+            if (result==="no winner")
+            {
+                round++;                                   // The fighters continue to next round
+                //Tell pop-up to display in the browser
+
+                alert(soilder1[0]+":"+soilder1[2]+"  *ROUND "+round+" OVER"+"* "+soilder2[0]+":"+soilder2[2]);
+                //alert(playerOneName+":"+playerOneHealth+"  *ROUND "+round+" OVER"+"*  "+playerTwoName+":"+playerTwoHealth);
+
+            } else{
+
+                alert(result);                             // Display pop-up in browser the result after each round
+                break;
+            }
+
+          }
+    }
+          //Run the "Winner Check" function If there is a "Winner" show winner If not End the round.
+    function winnerCheck(){
+        var result="no winner";
+                                   // No Winner variable is declared
+        if (soilder1[2]<1 && soilder2[2]<1)        // Condition statement that checks players health
+        {
+            result = "You Both Die";
+                                  // If Players health are both '0' than display result in pop-up
+        } else if(soilder1[2]<1){                      // Run the function to check Player #1 health
+            result = soilder2[0]+ "WINS!!!";               // If player #1 health is < 1 Display pop-up in browser player #2 wins
+        } else if (soilder2[2]<1)                      // If player #2 health is < 1 Display pop-up in browser player # 1 Wins
+        {
+            result = soilder1[0] +" WINS!!!"
+        }
+       return result;                                      //  Display pop-up in browser the "WINNER" of the game
+    }
+
+    /*******  The program gets started below *******/
+    fight();                                               //  After fight is over user can click button for another fight
+
+})();                                                      //  End function
--- goal3_assign_debug_v2/js/search.js
+++ goal3_assign_debug_v2/js/search.js
@@ -1,136 +1,145 @@
-/* Job Roberts
-28 November 2014
-PWA1
-Comments added
- */
 
- // Create privatized scope using a self-executing function
-(function() query{
-                                                          //Self executing function Main Function() run
+//Job Roberts
+//Date:29Nov2014
+//Class: PWA1
+//Assignment: ANALYZE Buggy Search v1
 
+// Create privatized scope using a self-executing function
+(function() {
+    console.log("Let's go find some JavaScript");
 
+    // Variable initialization (DO NOT FIX ANY OF THE BELOW VAR's)
+    var resultsDIV = document.getElementById("results"),
+        searchInput = document.forms[0].search,
+        currentSearch = '';
 
-	// Variable initialization (DO NOT FIX ANY OF THE BELOW 'Variables')
-	var resultsDIV = document.getElementById("results"),                 //Result variable is declared
-		searchInput = document.forms[0].search,                          //Declare search form
-		currentSearch = ''                                           //The form will be empty until user inputs a search query
+    // Validates search query
+    var isValid = function (query) {
+        console.log("Validate");
+        var valid = true;
 
-
-	// Validates search query
-	var validate==function(query){                                         // Validation variable declared
-
-
-		// Trim whitespace from start and end of search query
-		while(query.charAt(0) = " ") {                                    //
-            query = query.substring(1, query.length);                    // Start character extraction position (1)
-        };
-		while(query.charAt(query.length-1) === ""){
-            query = query.substring(0, query.length-1);                  //
-
+        // Trim whitespace from start and end of search query
         ;
 
-		// Check search length, must have 3 characters
-		if(query.length < 3) {                                             //Checks o validate character length
-            alert("Your search query is too small, try again.");          // User will be alerted that the length of the search is to small
+        // Check search length, must have 3 characters
+        if (query.length < 3) {
+            alert("Your search query is too small, try again.");
+            searchInput.focus();
+            valid = false
+        }
 
-            // (DO NOT FIX THE LINE DIRECTLY BELOW)
-            searchInput.focus();                                           // Allows user to enter a new search
-            return;
-        };
+        console.log("validate end");
+        return valid;
+    };
 
-                                                                 //Will alert user the return of the search from database
-        search(query);                                                     //Search initialized to database
-	};
+    // Finds search matches
+    var search = function (query) {
+        console.log("Search Function");
 
-	// Finds search matches
-	var search = function(query)                                        // Search variable declared
+        var i, dbLen,
+            j, qLen;
 
+        // split the user's search query string into an array
+        var queryArray = query.split(" ");
+        console.log(queryArray);
 
+        // array to store matched results from database.js
+        var results = [];
 
-		// split the user's search query string into an array
-		var queryArray = query.join(" ");                                   //Get query function
+        // loop through each index of db array
+        for (i = 0, dbLen = db.length; i < dbLen; i++) {
+            console.log(i);
+            // each db[i] is a single video item, each title ends with a pipe "|"
+            // save a lowercase variable of the video title
+            var dbTitleEnd = db[i].indexOf('|');
+            console.log('db[i]: ' + db[i]);
+            console.log('dbTitleEnd: ' + dbTitleEnd);
+            var dbItems = db[i].toLowerCase().substring(0, dbTitleEnd);
+            console.log(dbItems);
 
-		// array to store matched results from database.js
-		var results = [];                                                     //Result variable declared
+            // loop through the user's search query words
+            // save a lowercase variable of the search keyword
+            for (var j = 0, qLen = queryArray.length; j < qLen; j++) {
+                var qItem = queryArray[j].toLowerCase();
+                console.log('query ' + qItem);
 
-		// loop through each index of db array
-		for(var i=0, j=db.length; i<j; i++){                                   //For loop initialized to loop through Database
+                // is the keyword anywhere in the video title?
+                // If a match is found, push full db[i] into results array
+                var compare = dbItems.indexOf(qItem);
+                if (compare !== -1) {
+                    results.push(db[i]);
+                } else {
+                    console.log('No match found');
+                }
+            }
+        }
 
-			// each db[i] is a single video item, each title ends with a pipe "|"
-			// save a lowercase variable of the video title
-			var dbTitleEnd = db[i].indexOf('|');                                   //Data base title has ended.
-			var dbitem = db[i].toLowerCase().substring(0, dbTitleEnd);          //Database variable declared ,index the database
+        results.sort();
+        console.log('results');
+        console.log(results);
 
-			// loop through the user's search query words
-			// save a lowercase variable of the search keyword
-			for(var ii=0, jj=queryArray.length; ii<jj; ii++){                //Query will determine Character length
-				var qitem = queryArray[ii].toLowerCase();                  // Get query array
+        // Check that matches were found, and run output functions
+        if (results.length === 0) {
+            noMatch();
+        } else {
+            showMatches(results);
+        }
+    };
 
-				// is the keyword anywhere in the video title?
-				// If a match is found, push full db[i] into results array
-				var compare = dbitem.indexOf(qitem);                              //Search query will be compared to in the database
-				if(compare !== -1){                                               // Compares search throughout the database
-					results.push(db[i]);                                           //Print the results from database
-				};
+    // Put "No Results" message into page (DO NOT FIX THE HTML VAR NOR THE innerHTML)
+    var noMatch = function() {
+        var html = '' +
+                '<p>No Results found.</p>' +
+                '<p style="font-size:10px;">Try searching for "JavaScript".  Just an idea.</p>'
             ;
-        ;
+        resultsDIV.innerHTML = html;
+    };
 
-		results.sort();                                                             //Invoke result function print results from database
+    // Put matches into page as paragraphs with anchors
+    var showMatches = function (results) {
+        // THE NEXT 4 LINES ARE CORRECT.
+        var html = '<p>Results</p>',
+            title,
+            url;
 
-		// Check that matches were found, and run output functions
-		if(results.length = 0){                                                     //If result length equal 0 print NO MATCH!!
-			noMatch();
-		}else{                                                                      // Else if the results have a match print the matches!!
-			showMatches(results);                                                   //Prints out the results
-		};
-	};
+        // loop through all the results search() function
+        for (var i = 0, len = results.length; i < len; i++) {
+            // title of video ends with pipe
+            // pull the title's string using index numbers
+            console.log(results[i]);
+            var titleEnd = results[i].indexOf('|');
+            title = results[i].substring(0, titleEnd);
 
-	// Put "No Results" message into page (DO NOT FIX THE HTML VAR NOR THE innerHTML)
-	var noMatch = function(){                                                         //No match variable declared run function to browser
-    var html = ''+	                       //noinspection UnnecessaryLocalVariableJS
-                                                                    //browser variable declared
-			''<p>No Results found.</p>'+
-            //Print message to user
-			<p style="font-size:10px;">Try searching for "JavaScript" Just an idea"</p>
-		;
-		resultsDIV.innerHTML = html; //print results back to browser
-	};
+            // pull the video url after the title
+            url = results[i].substring(results[i].indexOf('|') + 1, results[i].length);
 
-	// Put matches into page as paragraphs with anchors
-	var showMatches = function(results){ //Run function to show matches
+            // make the video link - THE NEXT LINE IS CORRECT.
+            html += '<p><a href=' + url + '>' + title + '</a></p>';
 
-		// THE NEXT 4 LINES ARE CORRECT.
-		var html = <p>Results</p>, // Results are returned and print to browser
-			title,                 // Print the result title
-			url                     //Display result as a link
-		;
+            resultsDIV.innerHTML = html; //THIS LINE IS CORRECT.
+        }
 
-		// loop through all the results search() function
-		for(var i=0, j=results.length; i<j; i++){ //Run the result function determine length of word
+    };
 
-			// title of video ends with pipe
-			// pull the title's string using index numbers
-			titleEnd = results[i].indexOf('|');//Print the title of the result
-			title = results[i].subString(0, titleEnd);//After search has been indexed print title of result
+    console.log("Begin Search");
 
-			// pull the video url after the title
-			url = results[i].substring(results[i].indexOf('|')+1, results[i].length);//Print result with URL
+        // The onsubmit event will be reviewed in upcoming Course Material.
+        // THE LINE DIRECTLY BELOW IS CORRECT
+    document.forms[0].onsubmit = function(){
+        console.log('Submit');
 
-			// make the video link - THE NEXT LINE IS CORRECT.
-			html += '<p><a href=' + url + '>' + title + '</a></p>';// Each result will be linked to a URL
-		};
-		resultsDIV.innerHTML = html; //THIS LINE IS CORRECT.
-	};
+        var query = searchInput.value.trim();
+        //query = 'bug';
+        console.log('query search: \'' + query + '\'');
 
-	// The on submit event will be reviewed in upcoming Course Material.
-	// THE LINE DIRECTLY BELOW IS CORRECT
-	document.forms[0].onSubmit = function(){           // FORM declared invoke function
-		var query = searchInput.value;                  //Inout form for search input
-		validate(query);                               // Query will then be validated through input
+        if (isValid(query))
+            search(query);
+
+        // show results or no match messages
 
         // return false is needed for most events - this will be reviewed in upcoming course material
-        // THE LINE DIRECTLY BELOW IS CORRECT
-		return false;                     //If incorrect the return will print false
-    ;
 
-})();   //End program
+        // THE LINE DIRECTLY BELOW IS CORRECT
+        return false;
+    };
+})();
